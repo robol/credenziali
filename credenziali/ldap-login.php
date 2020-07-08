0505e1f10178e61a4ac9a0654311fc927f228cfc
Leonardo Robol <leo@robol.it>
2020-07-08 16:21:15 +0200

    Indentation, check TLS.

--- credenziali/ldap-login.php
+++ credenziali/ldap-login.php
@@ -10,30 +10,37 @@
  * Otherwise, the function returns null. 
  */
 function ldap_login($user, $pass) {
-        putenv("LDAPTLS_REQCERT=never");
+  putenv("LDAPTLS_REQCERT=never");
+
+  $ds = ldap_connect("ldap://idmauth.unipi.it");
+
+  if (! $ds) {
+    return null;
+  }
+  else {
+    ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+    
+    /* Start the TLS security layer -- if that does not work, 
+     * abort the authentication. */
+    if (! ldap_start_tls($ds)) {
+      return null;
+    }
+    
+    if (ldap_start_tls($ds)) {
+      $r = ldap_bind($ds, "uid=" . $user . ",dc=studenti,ou=people,dc=unipi,dc=it", $pass);
         
-        $ds = ldap_connect("ldap://idmauth.unipi.it");
-        
-        if (! $ds) {
-            return null;
-        }
-        else {
-            ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);  
-            if (ldap_start_tls($ds)) {
-                $r = ldap_bind($ds, "uid=" . $user . ",dc=studenti,ou=people,dc=unipi,dc=it", $pass);
-                
-                if ($r) {
-                   $results = ldap_search($ds, "dc=unipi,dc=it", "uid=" . $user);
-                   $matches = ldap_get_entries($ds, $results);
-                   $name = $matches[0]['cn'][0];
-                }
-            }
-            else {
-                return null;
-            }
-        }
-            
-       return [ 'user' => $user, 'name' => $name ];
+      if ($r) {
+        $results = ldap_search($ds, "dc=unipi,dc=it", "uid=" . $user);
+        $matches = ldap_get_entries($ds, $results);
+        $name = $matches[0]['cn'][0];
+      }
+    }
+    else {
+      return null;
+    }
+  }
+    
+  return [ 'user' => $user, 'name' => $name ];
 }
 
 ?>
