00ab606c5d6c6f08962d9320030f6e706850be18
Leonardo Robol <leo@robol.it>
2020-09-29 10:04:02 +0200

    Bump CSS version.

--- credenziali/index.php
+++ credenziali/index.php
@@ -28,7 +28,7 @@
 <head>
   <title>Download credenziali</title>
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="css/style.css?v=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
